d4de5f25dc835374c27ae698415d499f390e0755
Kodziak <kodziak1416@gmail.com>
2019-05-30 21:57:55 +0200

    Move project-card into component.

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="project-card">
+    <img class="project-card__img bor-rad-3" v-bind:src="project.img">
+    <div class="project-card__desc project-card--grid bor-rad-3">
+      <h2>{{ project.title }}</h2>
+      <div class="project-card__tags">
+        <div
+          v-for="(tag, index) in project.tags"
+          :key="index"
+          class="project-card__tag bor-rad-3"
+        >{{ tag }}</div>
+      </div>
+      <p>{{ project.description }}</p>
+      <div class="project-card__btn--row">
+        <a
+          v-if="project.demo"
+          target="_blank"
+          v-bind:href="project.demo"
+          rel="noopener noreferrer"
+          class="btn btn--view"
+        >demo</a>
+        <a
+          v-if="project.github"
+          target="_blank"
+          v-bind:href="project.github"
+          rel="noopener noreferrer"
+          class="btn btn--code"
+        >code</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    project: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -3,44 +3,21 @@
     <div>
       <h1>Projects</h1>
     </div>
-    <div class="project--flex">
-      <div v-for="(project, index) in projects" :key="index" class="project-card">
-        <img class="project-card__img bor-rad-3" v-bind:src="project.img">
-        <div class="project-card__desc project-card--grid bor-rad-3">
-          <h2>{{ project.title }}</h2>
-          <div class="project-card__tags">
-            <div
-              v-for="(tag, index) in project.tags"
-              :key="index"
-              class="project-card__tag bor-rad-3"
-            >{{ tag }}</div>
-          </div>
-          <p>{{ project.description }}</p>
-          <div class="project-card__btn--row">
-            <a
-              v-if="project.demo"
-              target="_blank"
-              v-bind:href="project.demo"
-              rel="noopener noreferrer"
-              class="btn btn--view"
-            >demo</a>
-            <a
-              v-if="project.github"
-              target="_blank"
-              v-bind:href="project.github"
-              rel="noopener noreferrer"
-              class="btn btn--code"
-            >code</a>
-          </div>
-        </div>
-      </div>
+    <div class="project--flex" v-if="projects.length">
+      <ProjectCard v-for="(project, index) in projects" :key="index" :project="project"/>
     </div>
+    <p v-else>Nothing to show...</p>
   </div>
 </template>
 
 <script>
+import ProjectCard from "./ProjectCard.vue";
+
 export default {
   name: "Projects",
+  components: {
+    ProjectCard
+  },
   data() {
     return {
       projects: [

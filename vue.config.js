6279841e4a22494d41ad6097ecd353ed4374bd73
Kodziak <kodziak1416@gmail.com>
2020-04-19 01:44:56 +0200

    Fix minor issues

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 /* eslint-disable import/prefer-default-export */
-// eslint-disable-next-line @typescript-eslint/no-var-requires
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 
 module.exports = {
